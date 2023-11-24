db6902f04b6b52f44046b99b3511c325f22587ba
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-11-24 16:57:50 +0100

    ADD - layout

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP ToDo List JSON</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <div id='app'>
+        <main>
+            <section>
+                <div class='container'>
+                    <h1>TO DO LIST</h1>
+                    <input type="text" placeholder='inserisci la task'>
+                    <ul>
+                        <li>task</li>
+                        <li>task</li>
+                        <li>task</li>
+                    </ul>
+                </div>
+            </section>
+        </main>
+    </div>
+</body>
+</html>
