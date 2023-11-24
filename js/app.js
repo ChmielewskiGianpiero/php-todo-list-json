9410358421e9e89d793daf5ac2f42e432ffdc2f2
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-11-24 17:45:00 +0100

    ADD - todos.json

--- js/app.js
+++ js/app.js
@@ -3,12 +3,18 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        
+        todos: []
       }
     },
     methods: {
         fetchData(){
-            axios.get('')
+            axios.get('./server.php').then(res => {
+                this.todos = res.data.results
+            }
+        )
         }
+    },
+    created() {
+        this.fetchData()
     }
   }).mount('#app')
--- server.php
+++ server.php
@@ -1,26 +1,14 @@
 <?php
 
-$todos = [
-    [
-    'text' => 'PHP',
-    'done' => false
-    ],
-    [
-    'text' => 'JavaScript',
-    'done' => false
-    ],
-    [
-    'text' => 'HTML',
-    'done' => true
-    ],
-    [
-    'text' => 'CSS',
-    'done' => true
-    ]
-    ];
+$todos_json = file_get_contents('./todos.json');
 
+$todos = json_decode($todos_json, true);
 
+$response = [
+    'success' => true,
+    'results' => $todos
+];
 
 header('Content-Type: application/json');
 
-echo json_encode($todos);
+echo json_encode($response);
